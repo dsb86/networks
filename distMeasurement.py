b5a7cecd8c71e54cf6a925c4261e508c6f6546b3
dsb86 <dsb86@case.edu>
2016-04-24 23:03:59 -0400

    inc

--- distMeasurement.py
+++ distMeasurement.py
@@ -3,7 +3,7 @@ import binascii
 local_ip = '127.0.0.1'
 local_port = 18675
 
-dest_ip = "129.22.108.21"
+dest_ip = socket.gethostbyname("www.google.com")
 dest_port = 34567
 
 
@@ -13,14 +13,14 @@ data = r'Lor dolor sit amet, consectetur adipiscing elit.Pellentesque id velit i
 icmp = socket.getprotobyname('icmp')
 udp = socket.getprotobyname('udp')
 ttl = 30
-#while True:
+
 # create receiving socket for icmp messages
 recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
 # create outgoing socket to send udp requests
 send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
 
 # modify outgoing socket to use desired ttl
-#send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
 
 # bind receiving socket to local port
 recv_socket.bind(("", local_port))
@@ -36,44 +36,14 @@ try:
     # recvfrom is blocking statement set to 4096 because assuming 1500b file possibility
     # from icmp message
     data_str, data_addr = recv_socket.recvfrom(4096)
-    '''
-    try:
-        # DNS Resolution
-        curr_name = socket.gethostbyaddr(data_addr)
-    except socket.error:
-        # Already Resolved
-        curr_name = data_addr
-    '''
-
 except socket.error:
-
     pass
 finally:
     send_socket.close()
     recv_socket.close()
-'''
-if data_addr is not None:
-    curr_host = "%s (%s)" % (curr_name, curr_addr)
-else:
-    curr_host = "*"
-print
-"%d\t%s" % (ttl, curr_host)
 
-ttl += 1
-if data_addr == dest_ip:
-    break
-'''
 print(data_str)
 msg_full=bin(int(binascii.hexlify(data_str), 16))
-msg_type=bin(int(binascii.hexlify(data_str[19:20]), 16))
-msg_code=bin(int(binascii.hexlify(data_str[22:24]), 16))
-msg_cs=bin(int(binascii.hexlify(data_str[24:27]), 16))
-msg_rest=data_str[27:]
 
-print("type: ", msg_type)
-print("code: ", msg_code)
-print("checksum: ", msg_cs)
-print(msg_rest)
-print(msg_full)
 
 
